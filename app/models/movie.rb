55773cc0cca0fb62cf50030c8ea692c51eb19e4f
Apoorv Singh <singhapo@msu.edu>
2015-03-30 11:25:08 -0400

    validation message fix

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,5 @@ class Movie < ActiveRecord::Base
   validates_presence_of :title, :rating
   validates :title, :format => { :with => /\A[a-zA-Z\d\s\W]*\z/,
     message: "only allows letters and numbers" }
-  validates :rating, :inclusion => {:in => 0..10, :message => "Rating must be between 0 and 10"}
+  validates :rating, :inclusion => {:in => 0..10, :message => "must be between 0 and 10"}
 end
