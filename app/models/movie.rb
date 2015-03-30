04ee4da4b448a97381414e991d6d0cef9e5ebf29
Apoorv Singh <singhapo@msu.edu>
2015-03-30 11:05:39 -0400

    indentation to spaces and validation changes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
-	validates_presence_of :title, :rating
-	validates :title, :format => { :with => /\A[a-zA-Z\d\s]*$\z/,
+  validates_presence_of :title, :rating
+  validates :title, :format => { :with => /\A[a-zA-Z\d\s\W]*\z/,
     message: "only allows letters and numbers" }
-	validates :rating, :inclusion => 0..10
+  validates :rating, :inclusion => 0..10, message: "Rating must be between 0 and 10"
 end
